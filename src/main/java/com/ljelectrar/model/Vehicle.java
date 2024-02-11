e244269d5bda65dec08d885463807151240e4bba
ljelectrar <ljelectrar@gmail.com>
2024-02-11 19:47:02 -0300

    Entities attributes added

--- src/main/java/com/ljelectrar/model/Vehicle.java
+++ src/main/java/com/ljelectrar/model/Vehicle.java
@@ -1,5 +1,69 @@
 package com.ljelectrar.model;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
 public class Vehicle {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 15)
+	private Status status;
+
+	@Column(length = 30)
+	private String owner;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(length = 30)
+	private Date associationDate;
+
+	public Vehicle() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public Date getAssociationDate() {
+		return associationDate;
+	}
+
+	public void setAssociationDate(Date associationDate) {
+		this.associationDate = associationDate;
+	}
 
 }
